1c093fd1cf849fbd1c83aef9190275e2c2a91294
RacsoSK <37522775+RacsoSK@users.noreply.github.com>
2018-07-04 10:24:13 -0500

    actualizacion temas y preguntas

---
+++ upTema.php
@@ -0,0 +1,46 @@
+<?php 
+/*	session_start();
+	if(!isset($_SESSION['matricula'])){
+		header("Location:index.html")
+	}*/
+ ?>
+ <!DOCTYPE html>
+ <html lang="es">
+     <head>
+         <meta charset="utf-8">
+         <title></title>
+         <meta name="description" content="">
+         <link rel="stylesheet" href="main.css">
+     </head>
+     <body>
+         <?php
+            //obtiene los valores del formulario generales
+            $idTema = $_POST['tema'];
+            $nomTema = $_POST['noPregunta'];
+
+            $host = "localhost";
+            $user = "root";
+            $pw = "";
+            $db = "proyecto";
+
+            $link = mysqli_connect($host,$user,$pw);
+            mysqli_select_db($link,$db);
+         
+            //Update de una pregunta
+           /* $consulta = mysqli_query($link,"SELECT * FROM preguntas WHERE id_Tema='$tema' AND noPregunta='$noPregunta'");            
+            if($row=mysqli_fetch_array($consulta)){
+                $p = $row['pregunta'];
+                echo $p;
+            }*/
+
+            
+            $sql = "UPDATE temas SET nombre_Tema='$nomTema' WHERE id_Tema='$idTema'";
+            //Script para validar la actualizacion
+            if (mysqli_query($link,$sql)) {
+                echo '<script>alert("Informacion agregada correctamente");</script>';
+                $link->close();
+                echo '<script>window.location="updateTemas.php";</script>';
+            }
+         ?>
+     </body>
+ </html>
--- updateTemas.php
+++ updateTemas.php
@@ -40,7 +40,54 @@
 
 		<!--PRESENTACION-->	
 		<div class="tema">
-			
+			<div class="contenedorTema">
+                <p id="titulo2">Listado de Todas las Preguntas</p>
+			    <form action="upTema.php" class="form_pregunta" method="post">
+			        
+			        <label for="pregunta" id="lpregunta"> ID Tema</label>
+			        <input type="text" id="tema" name="tema" placeholder="Numero de Tema"/>
+
+			        <label for="pregunta" id="lpregunta">Nuevo Tema</label>
+			        <input type="text" id="noPregunta" name="noPregunta" placeholder="Nuevo Tema"/>
+			        
+			        <input type="submit" id="submit" value="Enviar Cambios"/>
+			    </form>
+			    <?php
+                    $host = "localhost";
+                    $user = "root";
+                    $pw = "";
+                    $db = "proyecto";
+
+                    $link = mysqli_connect($host,$user,$pw);
+                     mysqli_select_db($link,$db);
+                    
+                    //echo utf8_encode();            
+                
+                    $consultaTema = mysqli_query($link,"SELECT * FROM temas");
+                    
+                    echo "<table>";
+                    echo "<tr>";
+                    echo "<th> ID Tema </th>";
+                    echo "<th> Tema </th>";
+                    echo "</tr>";
+                    
+                    while($fila = mysqli_fetch_array($consultaTema)){
+							
+						$id_tema = $fila['id_Tema'];
+						$nom_Tema = $fila['nombre_Tema'];
+
+						//RANGO, NUMERO DE PREGUNTAS CUYA RESPUESTA SON ETIQUETAS HTML, SE HACE LA CONVERSION DE HTML A TEXTO PLANO
+						//SI EL NUMERO DE PREGUNTA NO ES 3,5,7 NO HACE CONVERSION Y SOLO CODIFICA PARA QUE NO SALGAN COSAS RARAS
+
+                        
+                        echo "<tr>";
+                        echo "<td> $id_tema </td>";
+                        echo "<td> $nom_Tema </td>";
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                ?>
+			</div>
 		</div>
 
 	</body>

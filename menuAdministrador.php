49d0dbb2aa27bf86112209c9a89d1b811f5ef76f
RacsoSK <37522775+RacsoSK@users.noreply.github.com>
2018-07-04 00:03:45 -0500

    Php de administrador

---
+++ menuAdministrador.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title>Administrador</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/menuAdmin.css">
+		<link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Khand" rel="stylesheet">
+	</head>
+	<body>
+		<!--ENCABEZADO-->
+		<div class="titulo">
+			<header>
+				<h1>Generador de Examenes en Linea</h1>
+			</header>
+		</div>
+
+		<!--MENU-->
+		<div class="menu">
+			<nav>
+				<ul>
+					<li>
+						<a href="updateTemas.php"><h2>Actualizar Temas</h2></a>
+					</li>
+					<li>
+						<a href="updatePreguntas.php"><h2>Actualizar Preguntas</h2></a>
+					</li>
+					<li>
+						<a href="updateRespuestas.php"><h2>Actualizar Respuestas</h2></a>
+					</li>
+					<li>
+						<a href="listarAlumnos.php"><h2>Listar Alumnos</h2></a>
+					</li>
+					<li>
+						<a href="index.html"><h2>Cerrar Sesion</h2></a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+
+		<!--PRESENTACION-->	
+		<div class="graficos">
+			<p id="texto">
+			    ADMINISTRADOR
+			</p>
+		</div>
+
+	</body>
+</html>
---
+++ updateTemas.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title>Actualizar Temas</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/estilosAdmin.css">
+		<link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Khand" rel="stylesheet">
+	</head>
+	<body>
+		<!--ENCABEZADO-->
+		<div class="titulo">
+			<header>
+				<h1>Generador de Examenes en Linea</h1>
+			</header>
+		</div>
+
+		<!--MENU-->
+		<div class="menu">
+			<nav>
+				<ul>
+					<li>
+						<a href="updateTemas.php"><h2>Actualizar Temas</h2></a>
+					</li>
+					<li>
+						<a href="updatePreguntas.php"><h2>Actualizar Preguntas</h2></a>
+					</li>
+					<li>
+						<a href="updateRespuestas.php"><h2>Actualizar Respuestas</h2></a>
+					</li>
+					<li>
+						<a href="listarAlumnos.php"><h2>Listar Alumnos</h2></a>
+					</li>
+					<li>
+						<a href="index.html"><h2>Cerrar Sesion</h2></a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+
+		<!--PRESENTACION-->	
+		<div class="tema">
+			
+		</div>
+
+	</body>
+</html>

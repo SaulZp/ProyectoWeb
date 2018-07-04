bcf088cbac0260c2a121a745bc84a563e6c92846
SaulZp <32599800+SaulZp@users.noreply.github.com>
2018-07-03 22:17:30 -0500

    V3

--- login.php
+++ login.php
@@ -1,4 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Iniciar Sesion</title>
+        <meta name="description" content="">
+        <link rel="stylesheet" href="css/registroLogin.css">
+    </head>
+        <body>
+            
+            <!--CAJA DE TITULO-->
+            <div class="titulo">
+                <header>
+                    <h1>Generador de Examenes en Linea</h1>
+                </header>
+            </div>
+
+            <div class="login">
+                <h1>Inicio de Sesion</h1>
+
+                <form action="login.php" method="post">
+                    <label id="icon" for="matricula"></label>
+                    <input type="text" name="matricula" id="matricula" placeholder="Matricula" required autocomplete="off" />
+                    <label id="icon" for="pass"></label>
+                    <input type="password" name="pass" id="pass" placeholder="Contraseña" required autocomplete="off" />
+                    
+                    
+                    <div class="boton">
+                        <input type="submit" id="registrar" name="registrar" value="registrar">
+                    </div>
+                    <br>
+              </form>
+        </div>
+    
+    </body>
+</html>
+
+
+
 <?php 
+	//AQUI COMIENZA EL CODIGO PHP PARA VERIFICAR SI EL USUARIO ESTA REGISTRADO
+	error_reporting(0);
 	session_start();
 	$matricula = $_POST['matricula'];
 	$contrasena = $_POST['pass'];
@@ -34,13 +75,5 @@
 				</script>";
 			}	
 		}
-	/*	echo "<script type='text/javascript'>
-			var opcion = confirm('No te has registrado,¿Quieres crear una nueva cuenta como alumno?');
-			if(opcion==true){
-				window.location='registro.html';
-			}else{
-				window.location='login.html'
-			}			
-			</script>";*/
 	}
  ?>
